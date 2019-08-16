2780e5f9596527446d3e29b6733cb6300231c80c
jenny <jenn7nm@gmail.com>
2019-08-16 01:31:39 -0400

    deployment to heroku

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jenn/Documents/Flask_Blog/virtual"
+set -gx VIRTUAL_ENV "/home/jenn/Desktop/Flask_Blog/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
